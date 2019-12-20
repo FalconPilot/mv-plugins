acf610860ba90cf569e768961aa481d7fefd1d5e
Ralph' Potato <fpilot.github@gmail.com>
2019-12-20 16:02:43 +0100

    updated script with parameter

--- setAnimationFramerate.js
+++ setAnimationFramerate.js
@@ -1,5 +1,7 @@
 (function() {
   const pluginName = 'SetAnimationFramerate';
   const parameters = PluginManager.parameters(pluginName);
+  const fps = Number(parameters['Animations FPS'] || 15);
   console.log(parameters);
+  console.log(fps);
 })();
